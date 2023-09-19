44de3f4f4f6d556bd62e71e2f0e5868113826266
Dmitry <foxlife1023@gmail.com>
2023-09-19 22:53:44 +0300

    main

--- View/ProductView.php
+++ View/ProductView.php
@@ -30,34 +30,6 @@ class ProductView {
             </div>';
         }
     }
-    public function displayProductss($products) {
-        foreach ($products as $product) {
-            echo '
-            <div class="card">
-            <img src="../img/'.$product["img"].'.png" alt="img">
-                <div class="discrip">
-                <h2>' . $product["name"] . '</h2>
-                <h5>Цена: ' . $product["price"] . ' руб.</h5>
-                <h5>Кол-во: '.$product["count"].' шт.</h5>
-                <p>' . $product["description"] . '</p>
-                <div class="buttons">
-                    <form action="../Model/addingItemsToCart.php" method="post" enctype="multipart/form-data">
-                        <input style="display:none;" name="id" value='.$product["id"].'>
-                        <input style="display:none;" name="img" value='.$product["img"].'>
-                        <input style="display:none;" name="price" value='.$product["price"].'>
-                        <input style="display:none;" name="name" value='.$product["name"].'>
-                        <input type="number" max="'.$product["count"].'" min="1" name="count" value="1">
-                        <input type="submit" value="В корзину">
-                    </form>
-                    <form action="../View/ProductPage.php" method="post">
-                        <input style="display:none;" name="id" value='.$product["id"].'>
-                        <input type="submit" value="Подробнее">
-                    </form>
-                    </div>
-                </div>
-            </div>';
-        }
-    }
     // Изображение товара в корзине
     public function DisplayProductsInCart($products) {
         foreach ($products as $product) {
