25c63d8783129eabb73f261a07c44479032e76ef
Dmitry <foxlife1023@gmail.com>
2023-09-20 00:09:15 +0300

    main

--- View/Template/footer.php
+++ View/Template/footer.php
@@ -1,8 +1,10 @@
-<footer style="display: grid;">
-    <link rel="stylesheet" href="../View/css/styleFooter.css">
-    <a href="#">ВК</a>
-    <a href="#">Youtube</a>
-    <a href="#">Одноклассники</a>
-    <a href="#">Телеграмм</a>
-    <a href="#"></a>
-</footer>
+<body>
+    <footer style="display: grid;">
+        <link rel="stylesheet" href="../View/css/styleFooter.css">
+        <a href="#">ВК</a>
+        <a href="#">Youtube</a>
+        <a href="#">Одноклассники</a>
+        <a href="#">Телеграмм</a>
+        <a href="#"></a>
+    </footer>
+</body>
