2b139ce6f585fc38d988d8e8e6385f375b1aee30
Dmitry <foxlife1023@gmail.com>
2023-09-19 22:32:49 +0300

    main

--- Model/logout.php
+++ Model/logout.php
@@ -1,9 +1,7 @@
 <?
 if(session_status() === PHP_SESSION_NONE) session_start();
 
-session_unset();
 unset($_SESSION["user"]);
-$_SESSION["user"] = null;
 
 header('Location: ../View/index.php');
 ?>
